8dc57c197d1bdd7d62da6a2ed470151f981a7864
Nakul Mehra <nakulmehra0498@gmail.com>
2023-07-15 23:55:30 +0530

    day 3

--- check_default_value.java
+++ check_default_value.java
@@ -2,10 +2,11 @@ import java.util.*;
 
 class check_default_value
 {	
-	int b;
+	static int b; // make it class varible using static 
 	public static void main(String args[])
 	{
 		int a;
 		System.out.println(b);
+		System.out.println(a);	// it will give compilation error as we donot declare value of a .
 	}
 }
