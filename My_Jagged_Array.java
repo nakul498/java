aba7d5a507b6a1c73b043b96b860a21b7c5f65ca
Nakul Mehra <nakulmehra0498@gmail.com>
2023-07-22 23:54:36 +0530

    day7

--- My_Jagged_Array.java
+++ My_Jagged_Array.java
@@ -11,4 +11,27 @@ class My_Jagged_Array
 	{
 		System.out.print("\nEnter the subject name\t");
 		sc.nextLint();
-		subject [i
+		subject [i] = sc.nextLine();
+		System.out.println("Enter number of CA"+Subject[i]+"\t");
+		int ca = sc.nextINt();
+		marks[i] = new double[ca];
+		for(int j=0; j<ca; j++)
+		{
+			System.out.println("\nEnter Marks of CA"+(j+1)+"\t");
+			marks[i][j] = sc.nextDouble;
+		}
+	}
+	My_Jagged_Array ob= new My_Jagged_Array();
+	printResult(marks,subject);
+}
+Void printResult(double [][]m, String[]n)
+{
+	char grade[]=find grade(m);
+	System.out.println(\n\tResult - Jan 2021");
+	System.out.println("\t -------------------------------------");
+	for(i=0;i<m.length;i++)
+	{
+		System.out.prntln(n[i]+"\t\t");
+		for(int j=0;j<m[i].length;j++)
+		{
+			System.out.println(m[i][j
